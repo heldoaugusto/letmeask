9c5ff35f289942e0d6ffbb5d8ee5be5bd8886e65
heldosouza <heldoaugusto@gmail.com>
2021-06-26 20:56:20 -0300

    Inclusao de funcionalidades nas perguntas

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { ReactNode } from 'react';
+import classnames from 'classnames';
 
 import '../styles/question.scss';
 
@@ -8,12 +10,26 @@ type QuestionProps = {
     name: string;
     avatar: string;
   };
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
   children: ReactNode;
 };
 
-export function Question({ content, author, children }: QuestionProps) {
+export function Question({
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  children,
+}: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      className={classnames(
+        'question',
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered },
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
