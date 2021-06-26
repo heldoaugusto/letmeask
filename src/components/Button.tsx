16d811cba3c61832a124c0e8e89b92b913c703a6
heldosouza <heldoaugusto@gmail.com>
2021-06-25 22:38:26 -0300

    Criaçao de sala para administrador

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,17 @@ import { ButtonHTMLAttributes } from 'react';
 
 import '../styles/button.scss';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  // eslint-disable-next-line react/require-default-props
+  isOutlined?: boolean;
+};
 
-export function Button(props: ButtonProps) {
-  return <button type="button" className="button" {...props} />;
+export function Button({ isOutlined = false, ...props }: ButtonProps) {
+  return (
+    <button
+      type="button"
+      className={`button ${isOutlined && 'outlined'}`}
+      {...props}
+    />
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,7 +35,12 @@ export function Home() {
     const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
     if (!roomRef.exists()) {
-      alert('Room does not exists!');
+      alert('Essa sala não existe');
+      return;
+    }
+
+    if (roomRef.val().closedAt) {
+      alert('Esta sala foi encerrada!');
       return;
     }
 
